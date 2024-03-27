58f06c76156642133e2900f7b59c11b5d8431fc6
unknown <gouravbishnoi429@gmail.com>
2024-03-27 14:45:46 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+  }
+  function upside(){
+    document.getElementById("mini-svg").classList.toggle("translate_up")
+  }
